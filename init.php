cf32e3afce6c10a808055ad81618932812786369
Ryusuke SEKIYAMA <rsky0711@gmail.com>
2012-03-24 20:00:22 +0900

    Add composer.json

--- init.php
+++ init.php
@@ -1,4 +1,5 @@
 <?php
 set_include_path('phar://' . __DIR__ . '/p2pear.phar');
 error_reporting(E_ALL & ~(E_NOTICE | E_STRICT | E_DEPRECATED));
+require __DIR__ . '/vendor/.composer/autoload.php';
 require __DIR__ . '/conf/conf.inc.php';
