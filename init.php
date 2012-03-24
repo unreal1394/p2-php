6538ae87e88fde43444c5973976108066412c9cf
Ryusuke SEKIYAMA <rsky0711@gmail.com>
2012-03-25 00:12:01 +0900

    Change to use default include_path as fallback.

--- init.php
+++ init.php
@@ -1,5 +1,8 @@
 <?php
-set_include_path('phar://' . __DIR__ . '/p2pear.phar');
+if (file_exists(__DIR__ . '/p2pear.phar')) {
+    set_include_path('phar://' . __DIR__ . '/p2pear.phar'
+                     . PATH_SEPARATOR . get_include_path());
+}
 error_reporting(E_ALL & ~(E_NOTICE | E_STRICT | E_DEPRECATED));
 require __DIR__ . '/vendor/.composer/autoload.php';
 require __DIR__ . '/conf/conf.inc.php';
