4dd1c7f014107d3452becdab9f72355f36c7cf3c
heavenshell <heavenshell.jp@gmail.com>
2012-08-19 21:52:38 +0900

    Fix Composer autoload file path.

--- init.php
+++ init.php
@@ -4,5 +4,5 @@ if (file_exists(__DIR__ . '/p2pear.phar')) {
                      . PATH_SEPARATOR . get_include_path());
 }
 error_reporting(E_ALL & ~(E_NOTICE | E_STRICT | E_DEPRECATED));
-require __DIR__ . '/vendor/.composer/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/conf/conf.inc.php';
