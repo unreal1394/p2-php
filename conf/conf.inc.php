9a0b3912367bcd532893d17016991337e46f50e4
Ryusuke SEKIYAMA <rsky0711@gmail.com>
2013-03-13 02:10:36 +0900

    ver.130313.0210

--- conf/conf.inc.php
+++ conf/conf.inc.php
@@ -7,7 +7,7 @@
 // バージョン情報
 $_conf = array(
     'p2name'    => 'rep2-expack',   // rep2の名前
-    'p2version' => '130313.0030',   // rep2のバージョン
+    'p2version' => '130313.0210',   // rep2のバージョン
 );
 
 $_conf['p2ua'] = "{$_conf['p2name']}/{$_conf['p2version']}";
