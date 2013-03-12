41823235602b28eb08798e70a7f93fd1295cd0fc
Ryusuke SEKIYAMA <rsky0711@gmail.com>
2013-03-13 00:42:07 +0900

    p2pearディレクトリ or p2pear.pharをinclude_pathから除外 #16

--- init.php
+++ init.php
@@ -1,11 +1,4 @@
 <?php
-if (is_dir(__DIR__ . '/p2pear/includes')) {
-    set_include_path(__DIR__ . '/p2pear/includes'
-                     . PATH_SEPARATOR . get_include_path());
-} elseif (file_exists(__DIR__ . '/p2pear.phar')) {
-    set_include_path('phar://' . __DIR__ . '/p2pear.phar'
-                     . PATH_SEPARATOR . get_include_path());
-}
 error_reporting(E_ALL & ~(E_NOTICE | E_STRICT | E_DEPRECATED));
 require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/conf/conf.inc.php';
