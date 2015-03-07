dbcdb175749a57d34d82960bc56d844872c666ff
2ch774 <2ch774@users.noreply.github.com>
2015-03-07 12:34:51 +0900

    rep2-expack_allinone 150307.1230 名前を付けてみた。

--- conf/conf.inc.php
+++ conf/conf.inc.php
@@ -6,8 +6,8 @@
 
 // バージョン情報
 $_conf = array(
-    'p2name'    => 'rep2-expack_test',   // rep2の名前
-    'p2version' => '1140907.1420',   // rep2のバージョン
+    'p2name'    => 'rep2-expack_allinone',   // rep2の名前
+    'p2version' => '150307.1230',   // rep2のバージョン
 );
 
 $_conf['p2ua'] = "{$_conf['p2name']}/{$_conf['p2version']}";
