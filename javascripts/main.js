fa3676fee0c452f643971e8c7241156fbdf4587c
2ch774 <2ch774@users.noreply.github.com>
2015-04-02 18:29:26 +0900

    rep2のバージョンチェック機能に対応できるように変更

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,16 @@
-console.log('This would be the main JS file.');
+var xmlhttp = new XMLHttpRequest();
+var url = "version.txt";
+
+xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        var version = xmlhttp.responseText;
+        var out = "";
+        out += '<a class="zip_download_link" href="https://github.com/2ch774/p2-php/zipball/v' + version + '">Download this project as a .zip file</a>';
+        out += '<a class="tar_download_link" href="https://github.com/2ch774/p2-php/tarball/v' + version + '">Download this project as a tar.gz file</a>';
+
+        document.getElementById("downloads").innerHTML = out;
+    }
+}
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
+
